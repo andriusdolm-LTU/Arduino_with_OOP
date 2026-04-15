b4cceabfe48d60e15a42cdae9860d6d8d2e55da4
andriusdolm_LTU <andriusdolm@gmail.com>
2026-04-15 12:59:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,19 +22,66 @@ class Ismanus_Projektas:
         self.board = Arduino('COM5') 
         self.raud_led = Tuscias_LED()
         self.zal_led = Tuscias_LED()
-        self.mirksejimas = 'n'
-        self.laikas = 1
+        self._spalva = None
+        self._mirksejimas = None
+        self._laikas = None
+
+    @property
+    def spalva(self):
+        return self._spalva
+    
+    @spalva.setter
+    def spalva(self, value):
+        sutvarkyta_value = value.lower().strip()
+        leistinas = ['z', 'r', 'a']
+        if sutvarkyta_value in leistinas:
+            self._spalva = sutvarkyta_value
+        else:
+            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
+            self._spalva = None
+            time.sleep(2)
+    
+    @property
+    def mirksejimas(self):
+        return self._mirksejimas
+    
+    @mirksejimas.setter
+    def mirksejimas(self, value):
+        sutvarkytas_value = value.lower().strip()
+        leistinas = ['t', 'n']
+        if sutvarkytas_value in leistinas:
+            self._mirksejimas = sutvarkytas_value
+        else:
+            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
+            self._mirksejimas = None
+            time.sleep(2)
+
+    @property
+    def laikas(self):
+        return self._laikas
+    
+    @laikas.setter
+    def laikas(self, value):
+        if value >= 1 and value <=10:
+            self._laikas = value
+        else:
+            print("Negalima pasirinkti tokio laiko")
+            self._laikas = None
+            time.sleep(2)
 
     def klausimai(self):
-        spalva = input("Ar norite pajungti žalią, raudoną ar abu LED? (z/r/a)\n")
-        self.mirksejimas = input("Ar norite kad LED mirgsėtų ar ne? (t/n)\n")
+        while self.spalva is None:
+            self.spalva = input("Ar norite pajungti žalią, raudoną ar abu LED? (z/r/a)\n")
+        while self.mirksejimas is None:
+            self.mirksejimas = input("Ar norite kad LED mirgsėtų ar ne? (t/n)\n")
         if self.mirksejimas == 't':
-            self.laikas = float(input("Kas kiek laiko norite, kad mirksėtų? (1s iki 10s)\n"))
-        if spalva == 'z':
+            while self.laikas is None:
+                self.laikas = float(input("Kas kiek laiko norite, kad mirksėtų? (1s iki 10s)\n"))
+        if self.spalva == 'z':
             self.zal_led = LED(self.board, 13)
-        elif spalva == 'r':
+        elif self.spalva == 'r':
             self.raud_led = LED(self.board, 12)
-        elif spalva == 'a':
+        elif self.spalva == 'a':
             self.zal_led = LED(self.board, 13)
             self.raud_led = LED(self.board, 12)
 
