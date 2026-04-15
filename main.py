d8e5de868354e82f976e0a3104452fc659bf0196
andriusdolm_LTU <andriusdolm@gmail.com>
2026-04-15 12:26:24 +0300

    pakeistas main kodas

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import patch
 from pyfirmata import Arduino
 import time
+class Tuscias_LED:
+    def on(self): pass
+    def off(self): pass
+    
 
 class LED:
     def __init__(self, board, pin):
@@ -8,57 +12,61 @@ class LED:
 
     def on(self):
         self.pin.write(1)
-        print("LED įjungtas!")
 
     def off(self):
         self.pin.write(0)
-        print("LED išjungtas!")
-    
-    def time_on(self):
-        while True:
-            self.pin.write(1)
-            time(1)
     
 
 class Ismanus_Projektas:
     def __init__(self):
         self.board = Arduino('COM5') 
-        self.raud_led = None
-        self.zal_led = None
-    
+        self.raud_led = Tuscias_LED()
+        self.zal_led = Tuscias_LED()
+        self.mirksejimas = 'n'
+        self.laikas = 1
+
     def klausimai(self):
         spalva = input("Ar norite pajungti žalią, raudoną ar abu LED? (z/r/a)\n")
+        self.mirksejimas = input("Ar norite kad LED mirgsėtų ar ne? (t/n)\n")
+        if self.mirksejimas == 't':
+            self.laikas = float(input("Kas kiek laiko norite, kad mirksėtų? (1s iki 10s)\n"))
         if spalva == 'z':
-            self.zal_led = LED(self.board, 13,)
+            self.zal_led = LED(self.board, 13)
         elif spalva == 'r':
             self.raud_led = LED(self.board, 12)
         elif spalva == 'a':
             self.zal_led = LED(self.board, 13)
             self.raud_led = LED(self.board, 12)
-        funkcija = input("Ar norite kad LED mirgsetų ar ne? (t/n)\n")
 
         
-    def vykdymas_spalva(self):
-            if self.raud_led and self.zal_led:
+    def vykdymas_led(self):
+            if self.mirksejimas == 'n':
                 self.raud_led.on()
                 self.zal_led.on()
-            if self.raud_led and self.zal_led == None:
-                self.raud_led.on()
-            if self.zal_led and self.raud_led == None:
-                self.zal_led.on()
-            if not self.raud_led and not self.zal_led:
-                print("nepasirinkata")
-            input("užbaigti paspauskite Enter")
-            if self.raud_led:
+                input("norint užbaigti paspauskite ENTER")
                 self.raud_led.off()
-            if self.zal_led:
                 self.zal_led.off()
-            self.board.exit()
-    
-    def 
-            
+            elif self.mirksejimas == 't':
+                try:
+                    print("norint užbaigti paspauskite CTRL+C")
+                    while True:
+                        self.raud_led.on()
+                        self.zal_led.on()
+                        time.sleep(self.laikas/2)
+                        self.raud_led.off()
+                        self.zal_led.off()
+                        time.sleep(self.laikas/2)
+                except KeyboardInterrupt:
+                    pass
+        
+    def baigimas(self):
+        print("Išjungiami visi LED ir uždaroma jungtis")
+        self.raud_led.off()
+        self.zal_led.off()
+        self.board.exit()    
 
 if __name__ == "__main__":
     projektas = Ismanus_Projektas()
     projektas.klausimai()
-    projektas.vykdymas_spalva()
+    projektas.vykdymas_led()
+    projektas.baigimas()
