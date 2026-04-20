ae27058b9add57c63968c4380009d658fe278efc
andriusdolm_LTU <andriusdolm@gmail.com>
2026-04-20 20:32:47 +0300

    pridetas potenciometras ir naujas failas su idetu kodu is pagrindinio failo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import patch
 from pyfirmata import Arduino, util
+from nustatymai import Projekto_Nustatymai
 import time
+
 class Tuscias_LED:
     def on(self): pass
     def off(self): pass
@@ -17,78 +19,17 @@ class LED:
         self.pin.write(0)
     
 
-class Ismanus_Projektas:
+class Ismanus_Projektas(Projekto_Nustatymai):
     def __init__(self):
         self.board = Arduino('COM5')
         self.mygtukas = self.board.get_pin('d:2:i') 
+        self.potenciometras = self.board.get_pin('a:5:i')
         it = util.Iterator(self.board)
         it.start()
         self.zal_led = Tuscias_LED()
         self.gel_led = Tuscias_LED()
         self.raud_led = Tuscias_LED()
-        self._spalva = None
-        self._mirksejimas = None
-        self._laikas = None
-        self._mygtuko_funkcija = None
-    
-    @property
-    def spalva(self):
-        return self._spalva
-    
-    @spalva.setter
-    def spalva(self, value):
-        sutvarkyta_value = value.lower().strip()
-        leistinas = ['z', 'g', 'r', 'v']
-        if sutvarkyta_value in leistinas:
-            self._spalva = sutvarkyta_value
-        else:
-            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
-            self._spalva = None
-            time.sleep(2)
-    
-    @property
-    def mirksejimas(self):
-        return self._mirksejimas
-    
-    @mirksejimas.setter
-    def mirksejimas(self, value):
-        sutvarkytas_value = value.lower().strip()
-        leistinas = ['t', 'n']
-        if sutvarkytas_value in leistinas:
-            self._mirksejimas = sutvarkytas_value
-        else:
-            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
-            self._mirksejimas = None
-            time.sleep(2)
-
-    @property
-    def laikas(self):
-        return self._laikas
-    
-    @laikas.setter
-    def laikas(self, value):
-        if value >= 0.1 and value <=10:
-            self._laikas = value
-        else:
-            print("Negalima pasirinkti tokio laiko")
-            self._laikas = None
-            time.sleep(2)
-
-    @property
-    def mygtuko_funkcija(self):
-        return self._mygtuko_funkcija
-    
-    @mygtuko_funkcija.setter
-    def mygtuko_funkcija(self, value):
-        sutvarkytas_value = value.lower().strip()
-        leistinas = ['1', '2']
-        if sutvarkytas_value in leistinas:
-            self._mygtuko_funkcija = sutvarkytas_value
-        else:
-            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
-            self._mirksejimas = None
-            time.sleep(2)
-
+        
     def laukimas(self, sekundes = 0.01):
         pabaiga = time.time() + sekundes
         while time.time() < pabaiga:
@@ -97,14 +38,33 @@ class Ismanus_Projektas:
             time.sleep(0.005)
         return False
 
+    def laukimas_su_potenciometru(self):
+        pradzia = time.time()
+        while True:
+            if self.mygtukas.read() is True:
+                return True
+            potenciometro_reiksme = self.potenciometras.read()
+            if potenciometro_reiksme is None:
+                potenciometro_reiksmereiksme = 0.5
+            limitas = potenciometro_reiksme * 10
+            print(potenciometro_reiksme*10)
+            praejo = time.time() - pradzia
+            if praejo >= limitas:
+                return False
+            time.sleep(0.01)
+
+
     def klausimai(self):
         while self.spalva is None:
             self.spalva = input("Ar norite pajungti žalią, geltoną, raudoną ar visus tris LED? (z/g/r/v)\n")
         while self.mirksejimas is None:
             self.mirksejimas = input("Ar norite kad LED mirgsėtų ar ne? (t/n)\n")
         if self.mirksejimas == 't':
-            while self.laikas is None:
-                self.laikas = float(input("Kas kiek laiko norite, kad mirksėtų? (0.1s iki 10s)\n"))
+            while self.potenciometro_funkcija is None:
+                self.potenciometro_funkcija = input("Ar norite naudoti potenciometrą mirksėjimui ar ne? (1/2)\n")
+            if self.potenciometro_funkcija == '2':
+                while self.laikas is None:
+                    self.laikas = float(input("Kas kiek laiko norite, kad mirksėtų? (0.1s iki 10s)\n"))
         if self.mirksejimas == 'n':
             while self.mygtuko_funkcija is None:
                 self.mygtuko_funkcija = input("Ar norite junginėti LED su mygtuku ar tik ijungti? (1/2)\n")
@@ -121,27 +81,14 @@ class Ismanus_Projektas:
 
     def vykdymas_led(self):
             if self.mygtuko_funkcija == '2':
+                print("Norint užbaigti paspauskite mygtuka")
                 while True:
                     if self.mygtukas.read() is True:
                         break
-                    if self.mirksejimas == 'n':
-                        print("Norint užbaigti paspauskite mygtuka")
-                        self.zal_led.on()
-                        self.gel_led.on()
-                        self.raud_led.on()
-                    elif self.mirksejimas == 't':
-                        print("Norint užbaigti paspauskite mygtuka")
-                        while True:
-                            self.zal_led.on()
-                            self.gel_led.on()
-                            self.raud_led.on()
-                            if self.laukimas(self.laikas/2):
-                                break
-                            self.zal_led.off()
-                            self.gel_led.off()
-                            self.raud_led.off()
-                            if self.laukimas(self.laikas/2):
-                                break
+                    self.zal_led.on()
+                    self.gel_led.on()
+                    self.raud_led.on()
+                    time.sleep(0.001)
             elif self.mygtuko_funkcija == '1':
                 try:
                     print("Norint užbaigti paspauskite CTRL + C")
@@ -157,6 +104,42 @@ class Ismanus_Projektas:
                         time.sleep(0.01)
                 except KeyboardInterrupt:
                     print("Išjungta")
+            if self.potenciometro_funkcija == '2':
+                print("Norint užbaigti paspauskite mygtuka")
+                while True:
+                    self.zal_led.on()
+                    self.gel_led.on()
+                    self.raud_led.on()
+                    if self.laukimas(self.laikas/2):
+                        break
+                    self.zal_led.off()
+                    self.gel_led.off()
+                    self.raud_led.off()
+                    if self.laukimas(self.laikas/2):
+                        break
+            elif self.potenciometro_funkcija == '1':
+                print("Norint užbaigti paspauskite mygtuka")
+                while True:
+                    if self.mygtukas.read() is True:
+                        break
+                    potenciometro_reiksme = self.potenciometras.read()
+                    while potenciometro_reiksme < 0.005:
+                        potenciometro_reiksme = self.potenciometras.read()
+                        self.zal_led.off()
+                        self.gel_led.off()
+                        self.raud_led.off()
+                    self.zal_led.on()
+                    self.gel_led.on()
+                    self.raud_led.on()
+                    if self.laukimas_su_potenciometru():
+                        break
+                    self.zal_led.off()
+                    self.gel_led.off()
+                    self.raud_led.off()
+                    if self.laukimas_su_potenciometru():
+                        break
+                    time.sleep(0.01)
+
 
     def baigimas(self):
         print("Išjungiami visi LED ir uždaroma jungtis")
---
+++ nustatymai.py
@@ -0,0 +1,82 @@
+import time
+
+class Projekto_Nustatymai:
+    def __init__(self):
+        self._spalva = None
+        self._mirksejimas = None
+        self._potenciometro_funkcija = None
+        self._laikas = None
+        self._mygtuko_funkcija = None
+
+    @property
+    def spalva(self):
+        return self._spalva
+    
+    @spalva.setter
+    def spalva(self, value):
+        sutvarkyta_value = value.lower().strip()
+        leistinas = ['z', 'g', 'r', 'v']
+        if sutvarkyta_value in leistinas:
+            self._spalva = sutvarkyta_value
+        else:
+            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
+            self._spalva = None
+            time.sleep(2)
+    
+    @property
+    def mirksejimas(self):
+        return self._mirksejimas
+    
+    @mirksejimas.setter
+    def mirksejimas(self, value):
+        sutvarkytas_value = value.lower().strip()
+        leistinas = ['t', 'n']
+        if sutvarkytas_value in leistinas:
+            self._mirksejimas = sutvarkytas_value
+        else:
+            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
+            self._mirksejimas = None
+            time.sleep(2)
+
+    @property
+    def potenciometro_funkcija(self):
+        return self._potenciometro_funkcija
+    
+    @potenciometro_funkcija.setter
+    def potenciometro_funkcija(self, value):
+        sutvarkytas_value = value.strip()
+        leistinas = ['1', '2']
+        if sutvarkytas_value in leistinas:
+            self._potenciometro_funkcija = sutvarkytas_value
+        else:
+            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
+            self._potenciometro_funkcija = None
+            time.sleep(2)
+
+    @property
+    def laikas(self):
+        return self._laikas
+    
+    @laikas.setter
+    def laikas(self, value):
+        if value >= 0.1 and value <=10:
+            self._laikas = value
+        else:
+            print("Negalima pasirinkti tokio laiko")
+            self._laikas = None
+            time.sleep(2)
+
+    @property
+    def mygtuko_funkcija(self):
+        return self._mygtuko_funkcija
+    
+    @mygtuko_funkcija.setter
+    def mygtuko_funkcija(self, value):
+        sutvarkytas_value = value.strip()
+        leistinas = ['1', '2']
+        if sutvarkytas_value in leistinas:
+            self._mygtuko_funkcija = sutvarkytas_value
+        else:
+            print("Neteisingai įvestas atsakymas arba nėra tokio pasirinkimo")
+            self._mygtuko_funkcija = None
+            time.sleep(2)
